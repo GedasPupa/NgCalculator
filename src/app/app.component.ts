8cf0efb08b7866abff4afd4e2b7bbb373ffb6583
GedasPupa <gedosas@gmail.com>
2021-09-09 06:38:55 +0300

    initial commit + some first logic +/-

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'NgCalculator';
+  text = 'Placeholder text...';
+  number1 = 0;
+  number2 = 0;
+  output = 0;
+
+  sum() {
+    this.output = this.number1 + this.number2;
+  }
+  sub() {
+    this.output = this.number1 - this.number2;
+  }
+  mul() {
+    this.output = this.number1 * this.number2;
+  }
+  div() {
+    this.output = this.number1 / this.number2;
+  }
 }
+
+//      <!-- Dvikryptis: -->
+// <input [(ngModel)]='text'>
+// <p>{{ text }}</p>
+// <!-- 2 vienkrypičai -->
+// <input (input)="userInput($event)" [value]="text">
+// <p>{{ text }}</p>
